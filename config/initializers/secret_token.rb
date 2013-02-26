0688df9b1284d2aa6f4d845d9bb8223d3c40f802
Luis Dias <lcdias127@gmail.com>
2013-02-26 15:32:03 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joggingo::Application.config.secret_token = 'e0f3b0ee94df1aff2aab193903506a4dca387e86f830fce1b4c86a8735898546c4faf96b40bcc5d0dbf511d5ef44ffb6f36e6687c21b8f4e53ae048331d9fe37'
