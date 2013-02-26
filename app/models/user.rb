8ec67a7e4f523b22275b23881648218de9d4ed08
Luis Dias <lcdias127@gmail.com>
2013-02-26 17:59:02 +0000

    Validacoes dos campos do user done

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,20 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password_digest, :remember_token, :username
 
-  validates :name, :email, :username, presence: true
+  before_save { |user| user.email.downcase }
+
+  validates :name, presence: true, :length => {maximum: 40}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+[a-z]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+  					:uniqueness => {case_sensitive: false}, 
+  					:format => {with: VALID_EMAIL_REGEX}
+  validates :username, presence: true, :length => {maximum: 20},
+  				:uniqueness => {case_sensitive: false}
+
+  #validates :password, :presence => true, :length => {:minimum => 6}, 
+  #          :on => :create
+  #validates :password_confirmation, :presence => true, 
+  #          :on => :update, :unless => lambda{|user| user.password.blank?}
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,4 +31,104 @@ describe User do
 			should_not be_valid
 		end
 	end
+
+	describe 'name should not have more than 40 chars' do
+		
+		before do
+			@user.name = "a" * 41
+		end
+
+		it "should not be valid" do
+			@user.should_not be_valid 
+		end
+	end
+
+	describe "usernames should not have more than 20 chars" do
+		
+		before do
+			@user.username = "a" * 21
+		end
+
+		it "should not be valid" do
+			@user.should_not be_valid 
+		end
+	end
+
+	describe 'validate username uniqueness' do
+		
+		before do
+			user_with_same_username = @user.dup
+			user_with_same_username.email = "something@different.com"
+			user_with_same_username.save
+		end
+
+		it 'should not be valid' do		
+			@user.should_not be_valid
+		end
+
+		describe 'validate username uniqueness even with upcase' do
+
+			before do
+				user_with_same_username = @user.dup
+				user_with_same_username.email = "something@different.com"
+				user_with_same_username.username = @user.username.upcase
+				user_with_same_username.save
+			end
+
+			it 'should not be valid' do		
+				@user.should_not be_valid
+			end
+		end
+
+	end
+
+	describe 'validate email uniqueness' do
+		
+		before do
+			user_with_same_email = @user.dup
+			user_with_same_email.username = "somethingdifferent"
+			user_with_same_email.save
+		end
+
+		it 'should not be valid' do		
+			@user.should_not be_valid
+		end
+
+		describe 'validate email uniqueness even with upcase' do
+
+			before do
+				user_with_same_email = @user.dup
+				user_with_same_email.username = "somethingdifferent"
+				user_with_same_email.email = @user.email.upcase
+				user_with_same_email.save
+			end
+
+			it 'should not be valid' do		
+				@user.should_not be_valid
+			end
+		end
+
+	end
+
+	describe 'accept only valid emails' do
+
+		it "should be valid" do
+			example_emails = %w[user1@example.com user2@example.com user3@example.com]
+			example_emails.each do |valid_email|
+				@user.email = valid_email
+				@user.should be_valid				
+			end
+		end
+	end
+
+	describe 'dont accept invalid emails' do
+
+		it "should not be valid" do
+			example_emails = %w[user1@example user2example.com user@example..com]
+			example_emails.each do |invalid_email|
+				@user.email = invalid_email
+				@user.should_not be_valid
+			end
+		end
+	end
 end
